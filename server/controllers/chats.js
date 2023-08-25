426ad3cd75a4eafa3434e45a4ac56dbe4db1db27
Glenn Chiang <glennchiang24@gmail.com>
2023-08-25 15:17:49 +0800

    Wrote handlers for getting all user's chats and getting individual chat

--- server/controllers/chats.js
+++ server/controllers/chats.js
@@ -1,11 +1,29 @@
 const chatsRouter = require('express').Router()
+const User = require('../models/User')
+const Chat = require('../models/Chat')
 
 chatsRouter.get('/users/:userId/chats', async (req, res, next) => {
-  res.json()
+  try {
+    const user = await User.findById(req.params.userId).populate('chats')
+    if (!user) {
+      return res.status(404).json({error: 'User not found'})
+    }
+    res.json(user.chats)
+  } catch (error) {
+    next(error)
+  }
 })
 
 chatsRouter.get('/chats/:chatId', async (req, res, next) => {
-
+  try {
+    const chat = await Chat.findById(req.params.chatId).populate('messages').populate('users')
+    if (!chat) {
+      return res.status(404).json({error: 'Chat not found'})
+    }
+    res.json(chat)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = chatsRouter

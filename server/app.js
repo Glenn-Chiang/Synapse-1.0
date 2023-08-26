b50d8d9a7dcac2af770ffb0b637b7b67df6de333
Glenn Chiang <glennchiang24@gmail.com>
2023-08-26 16:57:07 +0800

    Integrated passport-local strategy for login, along with passport-jwt strategy to authenticate endpoints

--- server/app.js
+++ server/app.js
@@ -1,25 +1,31 @@
 const express = require('express')
+const app = express()
 require("dotenv").config();
 const cors = require('cors')
 const morgan = require('morgan');
 const mongoose = require("mongoose");
 const usersRouter = require('./controllers/users')
 const chatsRouter = require('./controllers/chats')
-const messagesRouter = require('./controllers/messages')
+const messagesRouter = require('./controllers/messages');
+const passport = require('passport');
 
+// Db connection
 const connectToDb = async () => {
   console.log("Connecting to MongoDB...");
   await mongoose.connect(process.env.MONGODB_URI);
   console.log("Connected to MongoDB");
 };
-
 connectToDb();
 
-const app = express()
+// Middleware
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 
+// Auth
+app.use(passport.initialize())
+
+// Routers
 app.use(usersRouter, chatsRouter, messagesRouter)
 
 module.exports = app
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,13 +1,54 @@
-const JwtStrategy = require('passport-jwt').Strategy
-const ExtractJwt = require('passport-jwt').ExtractJwt
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-const options = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'secret',
-}
+// Local strategy used for login
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    // username and password are sent in request body of login request
+    try {
+      const user = await User.findOne({ username });
 
-const authenticator = new JwtStrategy(options, (jwt, done) => {
+      if (!user) {
+        return done(null, false, { info: "User not found" });
+      }
 
-})
+      // Verify password
+      const passwordsMatch = await bcrypt.compare(password, user.passwordHash);
+      if (!passwordsMatch) {
+        return done(null, false, { info: "Incorrect password" });
+      }
 
+      // Success
+      return done(null, user);
+    } catch (error) {
+      return done(error, false);
+    }
+  })
+);
 
+// Jwt strategy used to secure endpoints
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.SECRET,
+    },
+    async (jwtPayload, done) => {
+      try {
+        const user = await User.findById(jwtPayload.id);
+        if (!user) {
+          return done(null, false, { info: "User not found" });
+        }
+        return done(null, user);
+      } catch (error) {
+        return done(error, false);
+      }
+    }
+  )
+);
+
+module.exports = passport;

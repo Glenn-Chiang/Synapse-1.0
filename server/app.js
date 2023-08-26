cd1cb15211c5c7ed10c572e6d48b77f920a75bcc
Glenn Chiang <glennchiang24@gmail.com>
2023-08-26 12:01:06 +0800

    Added morgan logging and connection to db

--- server/app.js
+++ server/app.js
@@ -1,12 +1,24 @@
 const express = require('express')
+require("dotenv").config();
 const cors = require('cors')
+const morgan = require('morgan');
+const mongoose = require("mongoose");
 const usersRouter = require('./controllers/users')
 const chatsRouter = require('./controllers/chats')
 const messagesRouter = require('./controllers/messages')
 
+const connectToDb = async () => {
+  console.log("Connecting to MongoDB...");
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("Connected to MongoDB");
+};
+
+connectToDb();
+
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(morgan('dev'))
 
 app.use(usersRouter, chatsRouter, messagesRouter)
 

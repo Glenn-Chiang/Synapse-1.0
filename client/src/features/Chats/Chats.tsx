f11b5370bc85965e5ab31488530fb6bdd6153e24
Glenn Chiang <glennchiang24@gmail.com>
2023-08-27 10:19:21 +0800

    Added 'no chats found' component to Chats which will be conidtionally rendered

--- client/src/features/Chats/Chats.tsx
+++ client/src/features/Chats/Chats.tsx
@@ -1,13 +1,12 @@
 import { faCommentDots, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Chat } from "../../types";
-import { Link } from "react-router-dom";
 import { useState } from "react";
 import SearchBar from "../../components/Searchbar";
 import { useQuery } from "react-query";
 import { getChats } from "../../requests/chats";
 import Loading from "../../components/Loading";
 import ErrorMessage from "../../components/ErrorMessage";
+import ChatPreview from "../../components/ChatPreview";
 
 function Chats() {
   const userId = localStorage.getItem("userId") as string;
@@ -51,31 +50,11 @@ function Chats() {
             </li>
           ))}
         </ul> :
-        <p className="p-2 shadow">No chats found</p>
+        <p className="p-4 shadow">No chats found</p>
       )}
     </main>
   );
 }
 
-function ChatPreview({ chat }: { chat: Chat }) {
-  const lastMessage = chat.messages[chat.messages.length - 1];
-  return (
-    <Link
-      to={`/chats/${chat.id}`}
-      className="p-2 h-20 w-full shadow flex gap-2 hover:bg-slate-200"
-    >
-      <div className="flex flex-col justify-between w-4/5">
-        <h2>{chat.users[1].username}</h2>
-        <p className="line-clamp-1 py-1">{lastMessage.text}</p>
-      </div>
-      <div className="flex flex-col justify-between items-end w-1/5">
-        <span>{lastMessage.timeStamp.toLocaleTimeString().split(" ")[0]}</span>
-        <span className="rounded-full bg-cyan-500 text-white w-8 h-8 flex justify-center items-center">
-          {chat.messages.length}
-        </span>
-      </div>
-    </Link>
-  );
-}
 
 export default Chats;

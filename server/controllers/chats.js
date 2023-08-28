f765f82686b8700eefb6f84e78de52ed1206b4f8
Glenn Chiang <glennchiang24@gmail.com>
2023-08-28 15:15:57 +0800

    Modified getChats route handler to directly retrieve Chat documents with users field containing user, instead of retrieving User document and populating chats field

--- server/controllers/chats.js
+++ server/controllers/chats.js
@@ -1,15 +1,14 @@
 const chatsRouter = require("express").Router();
-const User = require("../models/User");
+// const User = require("../models/User");
 const Chat = require("../models/Chat");
 
 // Get all user's chats
 chatsRouter.get("/users/:userId/chats", async (req, res, next) => {
   try {
-    const user = await User.findById(req.params.userId).populate("chats");
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    res.json(user.chats);
+    const chats = await Chat.find({ users: { $in: req.params.userId } })
+      .populate("messages")
+      .populate("users");
+    res.json(chats);
   } catch (error) {
     next(error);
   }
@@ -19,41 +18,16 @@ chatsRouter.get("/users/:userId/chats", async (req, res, next) => {
 chatsRouter.get("/chats/:chatId", async (req, res, next) => {
   try {
     const chat = await Chat.findById(req.params.chatId)
-      .populate("messages").populate({
+      .populate("messages")
+      .populate({
         path: "messages",
-        populate: "sender"
+        populate: "sender",
       })
       .populate("users");
-    
+
     res.json(chat);
   } catch (error) {
     next(error);
-  }
-});
-
-// Create chat
-chatsRouter.post("/chats", async (req, res, next) => {
-  try {
-    const user1 = await User.findById(req.query.user1);
-    const user2 = await User.findById(req.query.user2);
-    if (!user1 || !user2) {
-      return res.status(404).json({ error: `User not found` });
-    }
-  
-    const chat = new Chat({
-      users: [user1._id, user2._id],
-    });
-    const newChat = await chat.save();
-  
-    // Add chat to users' chats fields
-    user1.chats.push(newChat._id);
-    user2.chats.push(newChat._id);
-    await user1.save();
-    await user2.save();
-  
-    res.json(newChat);
-  } catch (error) {
-    next(error)
   }
 });
 

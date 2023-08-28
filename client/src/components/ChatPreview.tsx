9fe141577ce5245c12ef802a313bab2e18b5248d
Glenn Chiang <glennchiang24@gmail.com>
2023-08-28 15:17:57 +0800

    Updated ChatPreview to show username of other user in Chat

--- client/src/components/ChatPreview.tsx
+++ client/src/components/ChatPreview.tsx
@@ -2,23 +2,24 @@ import { Link } from "react-router-dom";
 import { Chat } from "../types";
 
 export default function ChatPreview({ chat }: { chat: Chat }) {
+  const currentUserId = localStorage.getItem("userId");
+  const username = chat.users.find(
+    (user) => user.id !== currentUserId
+  )?.username;
   const lastMessage =
-    chat.messages.length > 0 ? chat.messages[chat.messages.length - 1] : null;
+  chat.messages.length > 0 && chat.messages[chat.messages.length - 1];  
+
   return (
     <Link
-      to={`/chats/${chat.id}`}
+      to={`/chats/${chat.id}/${username}`}
       className="p-2 h-20 w-full shadow flex gap-2 hover:bg-slate-200"
     >
       <div className="flex flex-col justify-between w-4/5">
-        <h2>{chat.users[1].username}</h2>
+        <h2>{username}</h2>
         {lastMessage && <p className="line-clamp-1 py-1">{lastMessage.text}</p>}
       </div>
       <div className="flex flex-col justify-between items-end w-1/5">
-        {lastMessage && (
-          <span>
-            {lastMessage.timeStamp.toLocaleTimeString().split(" ")[0]}
-          </span>
-        )}
+        {lastMessage && <span>{lastMessage.timestamp}</span>}
         <span className="rounded-full bg-cyan-500 text-white w-8 h-8 flex justify-center items-center">
           {chat.messages.length}
         </span>

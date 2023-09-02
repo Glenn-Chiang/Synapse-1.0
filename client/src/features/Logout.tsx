293769afa00cb372bb139e2a1b1f82ba3acf8e29
Glenn Chiang <glennchiang24@gmail.com>
2023-09-02 14:51:12 +0800

    Built logout screen

--- client/src/features/Logout.tsx
+++ client/src/features/Logout.tsx
@@ -1,10 +1,26 @@
-import socket from "../socket"
+import socket from "../socket";
 
 export default function Logout() {
   const handleClick = () => {
-    localStorage.clear()
-    socket.disconnect()
-  }
+    localStorage.clear();
+    socket.disconnect();
+    console.log('Logged out')
+  };
 
-  return <button onClick={handleClick}>Logout</button>
+  return (
+    <main className="bg-slate-100 min-h-screen p-2 sm:p-8 flex items-center">
+      <section className="bg-white rounded-xl w-11/12 sm:w-4/5 m-auto drop-shadow-md ">
+        <header className="text-center p-4">
+          <h1 className="text-cyan-500 text-4xl text-center p-4">Synapse</h1>
+          <p className="pb-4 font-medium">Real-Time Messaging</p>
+        </header>
+        <p className="p-2 text-center">Are you sure you want to logout?</p>
+        <div className="flex justify-center p-4">
+          <button onClick={handleClick} className="p-2 bg-cyan-500 text-white rounded-md">
+            Logout
+          </button>
+        </div>
+      </section>
+    </main>
+  );
 }

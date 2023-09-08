f406c0eaea789d1bf6437a572f8ce14fa0ad77c4
Glenn Chiang <glennchiang24@gmail.com>
2023-09-08 12:00:12 +0800

    Developed Navbar

--- client/src/components/TopNav.tsx
+++ client/src/components/TopNav.tsx
@@ -1,42 +1,51 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  IconDefinition,
   faBoltLightning,
-  faPlus,
+  faComment,
+  faComments,
+  faEarth,
   faSignOut,
 } from "@fortawesome/free-solid-svg-icons";
-import CreateChannel from "../features/CreateChannel/CreateChannel";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export default function TopNav() {
-  const [showModal, setShowModal] = useState(false);
   return (
-    <>
-      {
-        showModal && <CreateChannel handleClose={() => setShowModal(false)} />
+    <nav className="z-20 fixed top-0 left-0 w-full h-16 flex items-center justify-between bg-cyan-500 text-white p-4">
+      <Link to={"/"}>
+        <h1 className="flex gap-2 items-center text-white text-2xl">
+          <FontAwesomeIcon icon={faBoltLightning} />
+          Synapse
+        </h1>
+      </Link>
+      <div className="flex gap-4">
+        <NavIcon to="/channels" label="My Channels" icon={faComments} />
+        <NavIcon to="/chats" label="My Chats" icon={faComment} />
+        <NavIcon to="/explore" label="Explore" icon={faEarth} />
+        <NavIcon to="logout" icon={faSignOut} />
+      </div>
+    </nav>
+  );
+}
+
+type NavIconProps = {
+  to: string;
+  label?: string;
+  icon: IconDefinition;
+};
+
+function NavIcon({ to, label: text, icon }: NavIconProps) {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        `flex gap-2 items-center px-4 py-2 rounded-full text-lg hover:bg-cyan-600 ${
+          isActive && "bg-cyan-600"
+        }`
       }
-      <nav className="z-20 fixed top-0 left-0 w-full h-16 flex items-center justify-between bg-cyan-500 text-white p-4 text-2xl">
-        <Link to={"/"}>
-          <h1 className="flex gap-2 items-center text-white">
-            <FontAwesomeIcon icon={faBoltLightning} />
-            Synapse
-          </h1>
-        </Link>
-        <div className="flex gap-4">
-          <button
-            onClick={() => setShowModal(true)}
-            className="flex items-center gap-2 text-xl"
-          >
-            <FontAwesomeIcon icon={faPlus} />
-            Create
-          </button>
-          <Link to={"/logout"}>
-            <h1 className="text-white">
-              <FontAwesomeIcon icon={faSignOut} />
-            </h1>
-          </Link>
-        </div>
-      </nav>
-    </>
+    >
+      <FontAwesomeIcon icon={icon} />
+      {text}
+    </NavLink>
   );
 }

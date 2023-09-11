650b155924c29891b190e285e0f817238252ecc3
Glenn Chiang <glennchiang24@gmail.com>
2023-09-11 15:04:11 +0800

    Added link back to homepage to Logout screen

--- client/src/features/Logout.tsx
+++ client/src/features/Logout.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import socket from "../socket";
 
 export default function Logout() {
@@ -12,17 +12,18 @@ export default function Logout() {
   };
 
   return (
-    <main className="bg-cyan-600 min-h-screen p-2 sm:p-8 flex items-center">
+    <main className="bg-slate-200 min-h-screen p-2 sm:p-8 flex items-center">
       <section className="bg-white rounded-xl w-11/12 sm:w-4/5 m-auto drop-shadow-md ">
         <header className="text-center p-4">
           <h1 className="text-cyan-500 text-4xl text-center p-4">Synapse</h1>
           <p className="pb-4 font-medium">Real-Time Messaging</p>
         </header>
         <p className="p-2 text-center">Are you sure you want to logout?</p>
-        <div className="flex justify-center p-4">
+        <div className="flex flex-col items-center justify-center p-4">
           <button onClick={handleClick} className="p-2 bg-cyan-500 text-white rounded-md">
             Logout
           </button>
+          <Link to={'/'} className="p-2 pt-4 text-cyan-500 hover:underline underline-offset-4">Back to Synapse</Link>
         </div>
       </section>
     </main>
